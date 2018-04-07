ef799a3363009118cb08cbc0464f70bc313a8f76
denimdatta <d.dat08@gmail.com>
2018-04-06 23:17:52 -0700

    Initial Checkin

---
+++ src/main/java/cse535/mobilecomputing/spring2018/group3/MainActivity.java
@@ -0,0 +1,13 @@
+package cse535.mobilecomputing.spring2018.group3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
