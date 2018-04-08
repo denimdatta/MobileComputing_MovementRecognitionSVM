a3f9ee27a051957c2cb4110259ed1fa31ea4923d
denimdatta <d.dat08@gmail.com>
2018-04-07 18:17:12 -0700

    SVM clasiffication activity

--- src/main/java/cse535/mobilecomputing/spring2018/group3/SvmParametersActivity.java
+++ src/main/java/cse535/mobilecomputing/spring2018/group3/SvmParametersActivity.java
@@ -2,6 +2,8 @@ package cse535.mobilecomputing.spring2018.group3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SvmParametersActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SvmParametersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_svm_parameters);
+
+        Button trainBtn = (Button) findViewById(R.id.TrainSVMBtn);
+        trainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
